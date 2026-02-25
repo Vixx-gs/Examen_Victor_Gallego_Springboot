8d8ece1c4d0fb1cd1d196bc67515cdc87b112921
Vixx-gs <victorgallegoserrate@gmail.com>
2026-02-25 17:00:31 +0100

    Naufrago hecho

--- src/main/java/com/salesianos/ExamenGallegoVictorDI/repository/NaufragoRepository.java
+++ src/main/java/com/salesianos/ExamenGallegoVictorDI/repository/NaufragoRepository.java
@@ -1,5 +1,15 @@
 package com.salesianos.ExamenGallegoVictorDI.repository;
 
-public class NaufragoRepository {
+import com.salesianos.ExamenGallegoVictorDI.model.Naufrago;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
+public interface NaufragoRepository extends JpaRepository<Naufrago, Long> {
+
+    @Query("SELECT n.nacionalidad, COUNT(n) FROM Naufrago n GROUP BY n.nacionalidad")
+    List<Object[]> countByNacionalidad();
+
+    @Query("SELECT n.sexo, COUNT(n) FROM Naufrago n GROUP BY n.sexo")
+    List<Object[]> countBySexo();
 }
